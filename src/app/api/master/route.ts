d7ee74d944059a8fd1a1d33e1b3c78495d4eed63
Andrey Zhukov <gudvin109@gmail.com>
2024-04-27 01:22:45 +0300

    main

--- src/app/api/master/route.ts
+++
@@ -1,7 +0,0 @@
-import Masters from '@/app/models/Master'
-import { NextResponse } from 'next/server'
-
-export async function GET(request: Request) {
-	const user = await Masters.findOne({})
-	return NextResponse.json(user)
-}
