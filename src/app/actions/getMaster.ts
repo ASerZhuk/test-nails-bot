279d083cc44593dff8e373a31498ad93fa93b720
Andrey Zhukov <gudvin109@gmail.com>
2024-04-27 02:46:27 +0300

    main

--- src/app/actions/getMaster.ts
+++ src/app/actions/getMaster.ts
@@ -2,7 +2,7 @@ import Masters from '../models/Master'
 
 export default async function getMaster() {
 	try {
-		const currentMaster = await Masters.findOne()
+		const currentMaster = await Masters.findOne().maxTimeMS(3000)
 		return currentMaster
 	} catch (error) {
 		console.error('Ошибка при получении мастера:', error)
