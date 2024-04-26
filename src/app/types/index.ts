a812538d8fb9574ccfe433f9e3771e9cbe880d83
Andrey Zhukov <gudvin109@gmail.com>
2024-04-27 01:14:15 +0300

    main

--- src/app/types/index.ts
+++ src/app/types/index.ts
@@ -1,7 +1,7 @@
-import { IMaster } from '../models/Master'
+import { IMasters } from '../models/Master'
 import { User } from '../models/User'
 
-export type SafeMaster = Omit<IMaster, 'createdAt' | 'updatedAt'> & {
+export type SafeMaster = Omit<IMasters, 'createdAt' | 'updatedAt'> & {
 	createdAt: Date
 	updatedAt: Date
 }
