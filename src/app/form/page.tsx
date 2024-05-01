eed44be4c302de42ad9420f89a355acba87256cb
Andrey Zhukov <gudvin109@gmail.com>
2024-05-02 00:30:58 +0300

    main

--- src/app/form/page.tsx
+++ src/app/form/page.tsx
@@ -2,7 +2,12 @@ import Container from '@/app/components/Container'
 import FormClient from './FormClient'
 
 const page = async () => {
-	const res = await fetch(`${process.env.NEXT_PUBLIC_API_BASE_URL}/api/master`)
+	const res = await fetch(
+		`${process.env.NEXT_PUBLIC_API_BASE_URL}/api/master`,
+		{
+			cache: 'no-store',
+		}
+	)
 	const data = await res.json()
 	return (
 		<Container>
