553ba5633ddaa7c5139fc33433fa713b2a0ef572
Andrey Zhukov <gudvin109@gmail.com>
2024-05-02 00:48:25 +0300

    main

--- src/app/form/page.tsx
+++ src/app/form/page.tsx
@@ -2,18 +2,31 @@ import Container from '@/app/components/Container'
 import FormClient from './FormClient'
 
 const page = async () => {
-	const res = await fetch(
-		`${process.env.NEXT_PUBLIC_API_BASE_URL}/api/master`,
-		{
-			cache: 'no-store',
+	try {
+		const res = await fetch(
+			`${process.env.NEXT_PUBLIC_API_BASE_URL}/api/master`,
+			{
+				cache: 'no-store',
+			}
+		)
+
+		if (res.ok) {
+			const data = await res.json()
+			return (
+				<>
+					<Container>
+						<FormClient currentMaster={data} />
+					</Container>
+				</>
+			)
+		} else {
+			console.error('Failed to fetch master:', res.status)
+			return null
 		}
-	)
-	const data = await res.json()
-	return (
-		<Container>
-			<FormClient currentMaster={data} />
-		</Container>
-	)
+	} catch (error) {
+		console.error('Failed to fetch master:', error)
+		return null
+	}
 }
 
 export default page
