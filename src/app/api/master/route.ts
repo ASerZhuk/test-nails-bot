501a694955169d06b2e145410b85afe33d1740f4
Andrey Zhukov <gudvin109@gmail.com>
2024-05-02 00:17:35 +0300

    main

--- src/app/api/master/route.ts
+++ src/app/api/master/route.ts
@@ -2,8 +2,7 @@ import dbConnect from '@/app/libs/dbConnect'
 import Masters from '@/app/models/Master'
 import { NextResponse } from 'next/server'
 
-export async function GET(request: Request) {
-	await dbConnect()
+export async function POST(request: Request) {
 	const master = await Masters.findOne({})
 	return NextResponse.json(master)
 }
