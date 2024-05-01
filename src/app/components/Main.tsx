2fea4f580cdd48c6581254e0e31f7cdedae77e9f
Andrey Zhukov <gudvin109@gmail.com>
2024-05-02 00:03:06 +0300

    main

--- src/app/components/Main.tsx
+++ src/app/components/Main.tsx
@@ -9,10 +9,13 @@ import { GrMoney } from 'react-icons/gr'
 import { SafeMaster } from '../types'
 import axios from 'axios'
 
-const Main = () => {
+interface MainProps {
+	currentMaster: SafeMaster | null
+}
+
+const Main: React.FC<MainProps> = ({ currentMaster }) => {
 	const [tg_id, setTg_Id] = useState()
 	const router = useRouter()
-	const [currentMaster, setCurrentMaster] = useState<SafeMaster>()
 	const dayOfWeekNames: Record<number, string> = {
 		0: 'Вск',
 		1: 'Пнд',
@@ -45,127 +48,110 @@ const Main = () => {
 		router.push(`/reservation_form`)
 	}
 
-	{
-		useEffect(() => {
-			const getMaster = async () => {
-				try {
-					const response = await axios.get(`/api/master?_=${Date.now()}`)
-					setCurrentMaster(response.data)
-				} catch (error) {
-					console.error('Failed to fetch master:', error)
-				}
-			}
+	return (
+		<>
+			<div className='flex flex-col items-center'>
+				<Avatar size={220} src={currentMaster?.image} />
 
-			if (!currentMaster) {
-				getMaster()
-			}
-		}, [])
-
-		return (
-			<>
-				<div className='flex flex-col items-center'>
-					<Avatar size={220} src={currentMaster?.image} />
-
-					<div
-						className='mt-8 text-2xl'
-						style={{ color: 'var(--tg-theme-text-color)' }}
-					>
-						{currentMaster?.firstName} {currentMaster?.lastName}
-					</div>
+				<div
+					className='mt-8 text-2xl'
+					style={{ color: 'var(--tg-theme-text-color)' }}
+				>
+					{currentMaster?.firstName} {currentMaster?.lastName}
 				</div>
+			</div>
 
-				<div className='flex justify-center mt-8'>
-					{currentMaster?.chatId === tg_id ? (
-						<>
-							<button
-								style={{
-									backgroundColor: 'var(--tg-theme-button-color)',
-									color: 'var(--tg-theme-button-text-color)',
-								}}
-								className='cursor-pointer p-2 pl-4 pr-4 rounded-full text-xs mt-2'
-								onClick={() => router.push('/form')}
-							>
-								Изменить профиль
-							</button>
-							<button
-								style={{
-									backgroundColor: 'var(--tg-theme-button-color)',
-									color: 'var(--tg-theme-button-text-color)',
-								}}
-								className='cursor-pointer p-2 pl-4 pr-4 rounded-full text-xs ml-4 mt-2'
-								onClick={() => router.push('/appointment_master')}
-							>
-								Запись ко мне
-							</button>
-						</>
-					) : (
+			<div className='flex justify-center mt-8'>
+				{currentMaster?.chatId === tg_id ? (
+					<>
 						<button
 							style={{
 								backgroundColor: 'var(--tg-theme-button-color)',
 								color: 'var(--tg-theme-button-text-color)',
 							}}
-							className='cursor-pointer p-2 pl-4 pr-4 rounded-full text-xs '
-							onClick={() => router.push('/appointment_client')}
+							className='cursor-pointer p-2 pl-4 pr-4 rounded-full text-xs mt-2'
+							onClick={() => router.push('/form')}
 						>
-							Мои записи
+							Изменить профиль
 						</button>
-					)}
-				</div>
+						<button
+							style={{
+								backgroundColor: 'var(--tg-theme-button-color)',
+								color: 'var(--tg-theme-button-text-color)',
+							}}
+							className='cursor-pointer p-2 pl-4 pr-4 rounded-full text-xs ml-4 mt-2'
+							onClick={() => router.push('/appointment_master')}
+						>
+							Запись ко мне
+						</button>
+					</>
+				) : (
+					<button
+						style={{
+							backgroundColor: 'var(--tg-theme-button-color)',
+							color: 'var(--tg-theme-button-text-color)',
+						}}
+						className='cursor-pointer p-2 pl-4 pr-4 rounded-full text-xs '
+						onClick={() => router.push('/appointment_client')}
+					>
+						Мои записи
+					</button>
+				)}
+			</div>
 
-				<hr
-					className='mt-8'
-					style={{ borderColor: 'var(--tg-theme-secondary-bg-color)' }}
-				/>
+			<hr
+				className='mt-8'
+				style={{ borderColor: 'var(--tg-theme-secondary-bg-color)' }}
+			/>
 
-				<div
-					className='mt-8 text-md flex flex-row justify-between'
-					style={{ color: 'var(--tg-theme-text-color)' }}
-				>
-					<div className='flex items-center'>
-						<HiOutlineCalendarDays size={28} className='pr-2' />
-						Рабочие дни:
-					</div>
-					<ul className='flex items-center'>
-						{availableDayNames.map(dayName => (
-							<li key={dayName} className='pr-2'>
-								{dayName}{' '}
-							</li>
-						))}
-					</ul>
+			<div
+				className='mt-8 text-md flex flex-row justify-between'
+				style={{ color: 'var(--tg-theme-text-color)' }}
+			>
+				<div className='flex items-center'>
+					<HiOutlineCalendarDays size={28} className='pr-2' />
+					Рабочие дни:
 				</div>
-				<hr
-					className='mt-8'
-					style={{ borderColor: 'var(--tg-theme-secondary-bg-color)' }}
-				/>
-				<div
-					className='mt-8 text-md flex flex-row justify-between'
-					style={{ color: 'var(--tg-theme-text-color)' }}
-				>
-					<div className='flex items-center'>
-						<BsWatch size={28} className='pr-2' />
-						Время работы:
-					</div>{' '}
-					<div>
-						с {currentMaster?.startTime} до {currentMaster?.endTime}
-					</div>
+				<ul className='flex items-center'>
+					{availableDayNames.map(dayName => (
+						<li key={dayName} className='pr-2'>
+							{dayName}{' '}
+						</li>
+					))}
+				</ul>
+			</div>
+			<hr
+				className='mt-8'
+				style={{ borderColor: 'var(--tg-theme-secondary-bg-color)' }}
+			/>
+			<div
+				className='mt-8 text-md flex flex-row justify-between'
+				style={{ color: 'var(--tg-theme-text-color)' }}
+			>
+				<div className='flex items-center'>
+					<BsWatch size={28} className='pr-2' />
+					Время работы:
+				</div>{' '}
+				<div>
+					с {currentMaster?.startTime} до {currentMaster?.endTime}
 				</div>
-				<hr
-					className='mt-8'
-					style={{ borderColor: 'var(--tg-theme-secondary-bg-color)' }}
-				/>
-				<div
-					className='mt-8 text-md flex flex-row justify-between'
-					style={{ color: 'var(--tg-theme-text-color)' }}
-				>
-					<div className='flex items-center'>
-						<GrMoney size={28} className='pr-2' />
-						Стоимость:
-					</div>
-					<div>{currentMaster?.price} руб.</div>
+			</div>
+			<hr
+				className='mt-8'
+				style={{ borderColor: 'var(--tg-theme-secondary-bg-color)' }}
+			/>
+			<div
+				className='mt-8 text-md flex flex-row justify-between'
+				style={{ color: 'var(--tg-theme-text-color)' }}
+			>
+				<div className='flex items-center'>
+					<GrMoney size={28} className='pr-2' />
+					Стоимость:
 				</div>
-			</>
-		)
-	}
+				<div>{currentMaster?.price} руб.</div>
+			</div>
+		</>
+	)
 }
 
 export default Main
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,10 +2,12 @@ import Container from '@/app/components/Container'
 import Main from './components/Main'
 
 const Home = async () => {
+	const res = await fetch(`${process.env.NEXT_PUBLIC_API_BASE_URL}/api/master`)
+	const data = await res.json()
 	return (
 		<>
 			<Container>
-				<Main />
+				<Main currentMaster={data} />
 			</Container>
 		</>
 	)
