9980e609c955fcb22b2ca12bcfdb373f90481bd9
Andrey Zhukov <gudvin109@gmail.com>
2024-05-03 03:18:15 +0300

    main

--- src/pages/api/deleteReservation/[id].ts
+++ src/pages/api/deleteReservation/[id].ts
@@ -9,33 +9,30 @@ export default async function handler(
 	if (req.method === 'DELETE') {
 		await dbConnect()
 		try {
-			const reservationId = req.query
+			const id = req.query
+			const reservationId = id.toString()
+
 			console.log(reservationId)
 
-			// Проверяем наличие идентификатора резервации
 			if (!reservationId || typeof reservationId !== 'string') {
 				throw new Error('Invalid reservation ID')
 			}
 
-			// Удаляем резервацию по идентификатору
 			const deletedReservation = await Reservation.deleteOne({
 				_id: reservationId,
 			})
 
-			// Проверяем, была ли успешно удалена резервация
 			if (!deletedReservation) {
 				return res.status(404).json({ error: 'Reservation not found' })
 			}
 
-			// Возвращаем успешный статус и удаленную резервацию
 			return res.status(200).json(deletedReservation)
 		} catch (error) {
 			console.error('Error deleting reservation:', error)
-			// Возвращаем статус ошибки
+
 			return res.status(500).json({ error: 'Failed to delete reservation' })
 		}
 	} else {
-		// Возвращаем ошибку метода, если запрос не является DELETE
 		return res.status(405).json({ error: 'Method Not Allowed' })
 	}
 }
